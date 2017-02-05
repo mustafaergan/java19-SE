1465bcc6d7ff29b45f556852b6e45a8f9a8839a1
vektorel <vektorel@lab8-ogr>
2017-02-05 15:32:00 +0200

    bitwse bakildi

---
+++ FirstProject/src/com/vektorel/bitwse/Islem.java
@@ -0,0 +1,12 @@
+package com.vektorel.bitwse;
+
+public class Islem {
+
+	public static void main(String[] args) {
+		int a = 15;
+		int b = 9;
+		System.out.println((a&b));
+		System.out.println((a|b));
+		System.out.println((a^b));
+	}
+}
