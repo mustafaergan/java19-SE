ad87a1011ab8e4176cd7702fc7896935f7cfc889
vektorel <vektorel@lab8-ogr>
2017-02-26 16:56:48 +0200

    swing

---
+++ FirstProject/src/com/swingvektorel/helloworld/Islem.java
@@ -0,0 +1,40 @@
+package com.swingvektorel.helloworld;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Islem {
+	
+	public static void main(String[] args) {
+		
+		JFrame jFrame = new JFrame("pencerem");
+		
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		jFrame.setSize(200, 200);
+		
+		JLabel jLabel = new JLabel("Hello World");
+		
+		JButton button = new JButton("Týkla");
+		
+		button.addActionListener( new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("týklandý");
+			}
+		});
+		
+		jFrame.add(button);
+		
+		jFrame.setLocationRelativeTo(null);
+		
+		jFrame.setVisible(true);
+		
+	}
+
+}
