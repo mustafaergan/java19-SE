55cd54c2de7570f44bb5868c0464fb861c72c699
vektorel <vektorel@lab8-ogr>
2017-03-05 13:59:50 +0200

    card

--- FirstProject/src/com/swingvektorel/layout/KartLayout.java
+++ FirstProject/src/com/swingvektorel/layout/KartLayout.java
@@ -3,13 +3,13 @@ package com.swingvektorel.layout;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class KartLayout extends JFrame{
 
@@ -20,37 +20,31 @@ public class KartLayout extends JFrame{
 		
 		CardLayout cardLayout = new CardLayout();
 		
-		JPanel jPanelButton = new JPanel(new FlowLayout());
+		JPanel jPanelAna = new JPanel(cardLayout);
 		
-		JButton button1 = new JButton("1");
-		JButton button2 = new JButton("2");
+		JPanel jPanelLogin = new JPanel(new GridLayout(3, 3));
+	
+		JPanel jPanelShowMessage = new JPanel(new FlowLayout());
 		
-		jPanelButton.add(button1);
-		jPanelButton.add(button2);
+		jPanelAna.add(jPanelLogin,"login");
 		
-		JPanel jPanelGoster = new JPanel(cardLayout);
+		jPanelAna.add(jPanelShowMessage,"message");
 		
-		jPanelGoster.add(new JLabel("1 Label"),"label1");
-		jPanelGoster.add(new JLabel("2 Label"),"label2");
+		JTextField jTextFieldAd = new JTextField(20);
+		JTextField jTextFieldSifre = new JTextField(20);
 		
-		add(jPanelButton,BorderLayout.NORTH);
-		add(jPanelGoster,BorderLayout.CENTER);
+		JButton jButton = new JButton("Giriþ");
 		
-		button1.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				cardLayout.show(jPanelGoster, "label1");
-			}
-		});
+		jPanelLogin.add(new JLabel("Kullanici Adi"));
+		jPanelLogin.add(jTextFieldAd);
+		jPanelLogin.add(new JLabel("Sifre"));
+		jPanelLogin.add(jTextFieldSifre);
+		jPanelLogin.add(new JLabel(""));
+		jPanelLogin.add(jButton);
 		
-		button2.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				cardLayout.show(jPanelGoster, "label2");
-			}
-		});
+		JLabel jLabelKarsilama = new JLabel("Hoþ Geldin");
 		
-		
+		jPanelShowMessage.add(jLabelKarsilama);
 		
 		setVisible(true);
 	}
